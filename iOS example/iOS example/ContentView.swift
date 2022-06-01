df7c1ae5f40acb3e882cb00a6778cdee80d06fd2
Alexander Kraev <kraev_as@utkonos.ru>
2022-06-01 14:06:11 +0300

    add-example init

---
+++ iOS example/iOS example/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  iOS example
+//
+//  Created by Alexander Kraev on 01.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello")
+    }
+}
